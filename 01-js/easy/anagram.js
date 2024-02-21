e080cc0e3d8ff431fd3b1cb195cbb32b98552d3b
aapush01 <mdshahidafridia31@gamil.com>
2024-02-21 16:15:49 +0530

    Add the solution

--- 01-js/easy/anagram.js
+++ 01-js/easy/anagram.js
@@ -5,7 +5,18 @@
 */
 
 function isAnagram(str1, str2) {
+  const s1 = str1.replace(/\s/g, "").toLowerCase();
+  const s2 = str2.replace(/\s/g, "").toLowerCase();
 
+  if (s1.length !== s2.length) {
+    return false;
+  }
+
+  //short the cahracter in both string
+  const sortedS1 = s1.split("").sort().join("");
+  const sortedS2 = s2.split("").sort().join("");
+
+  return sortedS1 === sortedS2;
 }
 
 module.exports = isAnagram;
