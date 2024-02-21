80793133d877f8f11b8300f5e11eb37b973f847f
aapush01 <mdshahidafridia31@gamil.com>
2024-02-21 23:11:43 +0530

    Medium level first que Solution

--- 01-js/medium/countVowels.js
+++ 01-js/medium/countVowels.js
@@ -6,7 +6,21 @@
 */
 
 function countVowels(str) {
-    // Your code here
+  // Your code here
+  const lowerCaseStr = str.toLowerCase();
+
+  //Array of vowels
+  const vowels = ["a", "e", "i", "o", "u"];
+
+  //use reduce to count occurence of vowels
+  const vowelCount = lowerCaseStr.split('').reduce((count, char) => {
+    if (vowels.includes(char)) {
+      return count + 1;
+    }
+    return count;
+  }, 0);
+
+  return vowelCount;
 }
 
 module.exports = countVowels;
